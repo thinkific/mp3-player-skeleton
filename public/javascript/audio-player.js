4a6f670a5ba57cfeb2df8d17a71e103c6a4a1ed4
Lance Barlaan <LPTBarlaan@gmail.com>
2017-03-02 19:15:39 -0800

    song playing, fixed container width for player, gave artwork container round corners

--- public/javascript/audio-player.js
+++ public/javascript/audio-player.js
@@ -1,5 +1,6 @@
 (function(w, m) {
   var player = function(element) {
+
     var self = this;
     this.wrapper = element;
     this.wrapper.append($("<audio>Your browser does not support the audio element.</audio>"));
@@ -25,18 +26,24 @@
     this.controlButton.on("click", function(e) {
       e.preventDefault();
       //implement me
+      if(self.isPlaying){
+        self.stop();
+      } else {
+        self.play();
+      }
     });
 
     self.stop();
   };
 
   player.prototype.initialize = function(song) {
-    this.jQueryElement.attr("src", song.url);
+    this.jQueryElement.attr("src", song[0].url);
     this.audioElement.load();
   };
 
   player.prototype.percentComplete = function(currentPlayTimeInSeconds) {
     //implement me
+    return Math.floor((currentPlayTimeInSeconds/this.duration)*100);
   };
 
   player.prototype.updateProgress = function() {
@@ -49,10 +56,18 @@
 
   player.prototype.play = function() {
     //implement me
+    this.isPlaying = true;
+    this.controlButton.removeClass("play-button");
+    this.controlButton.addClass("play-pause");
+    this.audioElement.play();
   };
 
   player.prototype.stop = function() {
     //implement me
+    this.isPlaying = false;
+    this.controlButton.addClass("play-button");
+    this.controlButton.removeClass("play-pause");
+    this.audioElement.pause();
   };
 
   player.prototype.formatDuration = function(seconds) {
